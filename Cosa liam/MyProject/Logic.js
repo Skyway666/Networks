b8f8efa1b3f2e9e08fc517892d609babf0cbbc92
Skyway666 <lucasgarciamateu@gmail.com>
2019-11-09 20:15:31 +0100

    Refracted some code + concatenated body parts

--- Cosa liam/MyProject/Logic.js
+++ Cosa liam/MyProject/Logic.js
@@ -1,74 +1,45 @@
 
 
-
-
 // Input handling
 var search_bar = document.getElementById("search_bar")
 
-//enum
+// Filter and order vars
 var filter_criteria = 0
 var filter_criterias =["employee", "datecreated", "location", "injurytype"]
-//enum
 var ordering_criterias = ["desc","asc"] 
 var ordering_criteria = 0
 
-var finished_requests = 0
-
-function makeQuery(){
-
-  // Clean arrays
-
-  incidents = []
-  injury_types = []
-  locations = []
-  tableData = []
-  
-
-  // Make a request to the API
-    
-   getIncidents(search_bar.value, filter_criterias[filter_criteria], ordering_criterias[ordering_criteria])
-   // TODO: Optimization- If the arrays are already ordered in a descending way, no need to get them again
-   getEmployees()
-   getInjuryTypes()
-   getLocations()
-   getBodyParts()
-  
-}
-
-function changeFilter(){
-  
-  filter_criteria += 1
-  if(filter_criteria > 3) filter_criteria = 0
-
-  filterDynamic.filter_criteria = filter_criterias[filter_criteria]
-
-  makeQuery()
-
-}
-
-function changeOrdering(){
-
-  ordering_criteria += 1
-  if(ordering_criteria > 1) ordering_criteria = 0
-
-  filterDynamic.ordering_criteria = ordering_criterias[ordering_criteria]
-
-  makeQuery()
-}
-
-
-// VARIABLES
+// Data from querys vars
 var incidents = {} // Contains all the id's
 
-
-var employees = {} // All employees THAT EXIST
-var injury_types = {} //All injuries THAT EXIST
-var locations = {} // All locations THAT EXIST
-var bodyParts = {} // All body parts THAT EXIST
+var employees = [] // All employees THAT EXIST
+var injury_types = [] //All injuries THAT EXIST
+var locations = [] // All locations THAT EXIST
+var bodyParts = [] // All body parts THAT EXIST
 
 var tableData = []
 
+// VUE 
+
+var tableDynamic = new Vue({
+  // Vue variables... i guess+
+  el: '#table',
+  data:{
+    tableData: tableData
+  }
+})
+
+var filterDynamic = new Vue({
+  el: '#search_criteria',
+  data:{
+    filter_criteria: filter_criterias[filter_criteria],
+    ordering_criteria: ordering_criterias[ordering_criteria]
+  }
+})
+
 // REQUESTS
+
+var finished_requests = 0
 var incidents_request = new XMLHttpRequest() 
 
 var employees_request = new XMLHttpRequest() 
@@ -100,6 +71,13 @@ bodyParts_request.onload = function(){
 }
 
 
+// search_bar -> oninput=
+function makeQuery(){
+  // Make a request to the API
+   getIncidents(search_bar.value, filter_criterias[filter_criteria], ordering_criterias[ordering_criteria])
+}
+
+
 // This functions will fill variables
 function getIncidents(filter, filter_crit, ordering_crit){
   var request = "http://localhost:3000/api/v1/incident";
@@ -109,8 +87,7 @@ function getIncidents(filter, filter_crit, ordering_crit){
   request = request.concat("&sort=" + filter_crit)
   request = request.concat("&sortdir=" + ordering_crit)
   }
-
-  console.log(request)
+  
   incidents_request.open('GET', request , true)
   incidents_request.send()
 }
@@ -127,23 +104,56 @@ function getLocations(){
   locations_request.open('GET', 'http://localhost:3000/api/v1/location', true)
   locations_request.send()
 }
-
 function getBodyParts(){
   bodyParts_request.open('GET', 'http://localhost:3000/api/v1/bodyPart', true)
   bodyParts_request.send()
 }
 
 
-// This functions will use variables to confortably fill the table
+
+// filter_button -> onclick=
+function changeFilter(){
+  
+  filter_criteria += 1
+  if(filter_criteria > 3) filter_criteria = 0
+
+  filterDynamic.filter_criteria = filter_criterias[filter_criteria]
+
+  makeQuery()
+
+}
+
+// ordering_button -> onclick=
+function changeOrdering(){
+
+  ordering_criteria += 1
+  if(ordering_criteria > 1) ordering_criteria = 0
+
+  filterDynamic.ordering_criteria = ordering_criterias[ordering_criteria]
+
+  makeQuery()
+}
+
+
+
+// This functions will use variables to confortably fill the tableData
 function createTableData(){
+
+  tableData = []
+
   incidents.forEach(incident => {
     var data = {}
     data.date = incident.dateCreated
     data.employee = getDataById(employees, incident.employeeId).fullName
     data.injury_type = getDataById(injury_types, incident.injuryTypeId).name
     data.location = getDataById(locations, incident.locationId).name
-    data.bodyParts = getDataById(bodyParts, incident.bodyPartIds[0]).name // TODO: Functionality - get all body parts
-
+    
+    // First body part
+    data.bodyParts = getDataById(bodyParts, incident.bodyPartIds[0]).name
+    // Concat all body parts in bodyPartsIds
+    for(var i = 1; i < incident.bodyPartIds.length;i++ )
+      data.bodyParts = data.bodyParts.concat(", " + getDataById(bodyParts, incident.bodyPartIds[i]).name) 
+    
     tableData.push(data)
     });
 
@@ -155,51 +165,34 @@ function getDataById(array, id){
 
   array.forEach(instance => {
     if(instance.id == id){
-      ret = instance
       // TODO: Optimization - stop loop when we find
+      ret = instance
     }
   })
   return ret
 }
-// Loop function to wait for all requests to return
+
+
 
 //Start :)
 
+
+getEmployees()
+getInjuryTypes()
+getLocations()  
+getBodyParts()
 makeQuery()
 
+window.requestAnimationFrame(update)
+
+// Loop function to wait for all requests to return
 function update(){
   if(finished_requests == 5){
-    finished_requests = 0
+    finished_requests = 4
     createTableData()
   }
 
   window.requestAnimationFrame(update)
 }
-
-
-// What is executed once
-window.requestAnimationFrame(update)
-
-
-
-// VUE TESTING
-
-var tableDynamic = new Vue({
-  // Vue variables... i guess+
-  el: '#table',
-  data:{
-    tableData: tableData
-  }
-})
-
-var filterDynamic = new Vue({
-  el: '#search_criteria',
-  data:{
-    filter_criteria: filter_criterias[filter_criteria],
-    ordering_criteria: ordering_criterias[ordering_criteria]
-  }
-})
-
-
 
 //https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/ thanks girl
